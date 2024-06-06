25a67ec5d50ca2c5e4183a34ebdfdba7bd8f64d1
Adam Crayton <adamcrayton@u.boisestate.edu>
2024-06-06 15:07:32 -0600

    Added more Documentation

--- utils/cfg.py
+++ utils/cfg.py
@@ -2,8 +2,13 @@ from yacs.config import CfgNode as CN
 from utils.utils import log_msg
 
 
-# simplify cfg
+# This function simplified the CfgNode and sets each part of the dump_cfg to simplify the passed in cfg
+#
+# args - parsed arguments from main
+# cfg - the CfgNode that we are simplifying
+# return - returns the simplified CfgNode
 def simplify_cfg(args, cfg):
+    # Gets the general information of the original CfgNode and applies it to the dump_cfg
     dump_cfg = CN()
     dump_cfg.DATASET = cfg.DATASET
     dump_cfg.OPTIMIZER = cfg.OPTIMIZER
@@ -22,6 +27,7 @@ def simplify_cfg(args, cfg):
         dump_cfg['Local'][cfg[args.method].local_method] = CN()
         dump_cfg['Local'][cfg[args.method].local_method] = cfg['Local'][cfg[args.method].local_method]
 
+    # Gets any information of the attack_type in the original CfgNode, if applicable
     if args.attack_type != 'None':
         dump_cfg['attack'] = CN()
         dump_cfg['attack'].bad_client_rate = cfg['attack'].bad_client_rate
@@ -33,7 +39,11 @@ def simplify_cfg(args, cfg):
         
     return dump_cfg
 
-
+# Shows the information of a CfgNode
+#
+# args - the parsed arguments from main
+# cfg - the CfgNode
+# method - the method attached to the CfgNode that we are reporting
 def show_cfg(args, cfg, method):
     dump_cfg = CN()
     dump_cfg.DATASET = cfg.DATASET
