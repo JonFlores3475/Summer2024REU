f78238589ab523fc279bf7a5e82ce12ea9a5bc18
egkendall <147090431+egkendall@users.noreply.github.com>
2024-06-10 13:49:38 -0600

    Adding a demo sympy partial derivative

--- Attack/Poisoning Attack/utils.py
+++ Attack/Poisoning Attack/utils.py
@@ -5,6 +5,21 @@ import keras
 import torch
 from torch.utils.data import DataLoader
 
+# --------------------------------
+# import sympy 
+from sympy import *
+ 
+x, y = symbols('x y')
+expr = x**2 + 2 * y + y**3
+print("Expression : {}".format(expr))
+  
+# Use sympy.Derivative() method 
+expr_diff = Derivative(expr, x)  
+     
+print("Derivative of expression with respect to x : {}".format(expr_diff))  
+print("Value of the derivative : {}".format(expr_diff.doit()))
+# --------------------------------
+
 def inverse_loss(target, prediction):
     loss = keras.categorical_crossentropy(target)
     if loss < 0.001:
