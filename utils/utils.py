b9ee4344bfe91e06b90aaac7a450cb6ab61992a1
Adam Crayton <adamcrayton@u.boisestate.edu>
2024-06-20 14:49:07 -0600

    m

--- utils/utils.py
+++ utils/utils.py
@@ -123,7 +123,7 @@ def row_into_parameters(row, parameters):
             param.data[:] = torch.reshape(torch.tensor(current_data), param.shape)
             offset += new_size
     else:
-            new_size = functools.reduce(lambda x, y: x * y, parameters)
+            new_size = parameters * parameters
             current_data = row[offset:offset + new_size]
 
             param.data[:] = torch.reshape(torch.tensor(current_data), parameters)
