7ab34f37247073e1cf0699e4895c9e9767c78a7e
Jon Flores <jonathanflores@borah-login.cm.cluster>
2024-07-01 10:09:12 -0600

    Base and FedProx should work with all attacks now.

--- Methods/FedProx.py
+++ Methods/FedProx.py
@@ -14,11 +14,11 @@ class FedProx(FederatedMethod):
     def ini(self):
         super().ini()
 
-    def local_update(self, priloader_list):
+    def local_update(self, priloader_list, losses = []):
         total_clients = list(range(self.cfg.DATASET.parti_num))
         self.online_clients_list = self.random_state.choice(total_clients, self.online_num, replace=False).tolist()
 
-        self.local_model.loc_update(online_clients_list=self.online_clients_list,
+        self.local_model.loc_update(losses, online_clients_list=self.online_clients_list,
                                     nets_list=self.nets_list, global_net=self.global_net,
                                     priloader_list=priloader_list)
 
